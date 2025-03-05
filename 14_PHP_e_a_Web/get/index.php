21bd3d9538d078a7810a7d4f4d7e1abc8d99f406
Gabriel Silveira Ildefonso <ssgabrielvinicius@gmail.com>
2025-03-05 07:41:22 -0300

    atualização

---
+++ 14_PHP_e_a_Web/get/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Método Get</title>
+</head>
+
+<body>
+  <form action="processamento.php" method="GET">
+    <div>
+      <input type="text" name="nome" placeholder="Digite seu nome">
+    </div>
+    <div>
+      <input type="submit" value="Enviar">
+    </div>
+</body>
+
+</html>
