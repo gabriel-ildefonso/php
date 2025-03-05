298681b9d722ddec9b2a3eb288612253124cd3fa
Gabriel Silveira Ildefonso <ssgabrielvinicius@gmail.com>
2025-03-05 08:13:30 -0300

    exemplo de método get

--- 14_PHP_e_a_Web/get/index.php
+++ 14_PHP_e_a_Web/get/index.php
@@ -13,6 +13,9 @@
       <input type="text" name="nome" placeholder="Digite seu nome">
     </div>
     <div>
+      <input type="text" name="idade" placeholder="Digite sua idade">
+    </div>
+    <div>
       <input type="submit" value="Enviar">
     </div>
 </body>
